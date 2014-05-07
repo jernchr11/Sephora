4b4aa75bfb42e7673bba9c9c6343b23914a45d7c
jernchr11 <jernchr@berkeley.edu>
2014-05-06 19:49:09 -0700

    ...

--- tutorial/tutorial/spiders/sephora_spider.py
+++ tutorial/tutorial/spiders/sephora_spider.py
@@ -7,13 +7,14 @@ from tutorial.items import SephoraItem
 class SephoraSpider(Spider):
     name = "sephora"
     allowed_domains = ["http://www.sephora.com"]
-    start_urls = [
-        "http://www.sephora.com/makeup-primer-face-primer?defaultSortBy=-1&pageSize=-1", "http://www.sephora.com/foundation-makeup?defaultSortBy=-1&pageSize=-1",
-        "http://www.sephora.com/bb-cc-cream-face-makeup?defaultSortBy=-1&pageSize=-1", "http://www.sephora.com/foundation-sets?defaultSortBy=-1&pageSize=-1",
-        "http://www.sephora.com/setting-powder-face-powder?defaultSortBy=-1&pageSize=-1", "http://www.sephora.com/concealer?defaultSortBy=-1&pageSize=-1",
-        "http://www.sephora.com/blush-face-makeup?defaultSortBy=-1&pageSize=-1", "http://www.sephora.com/bronzer-makeup?defaultSortBy=-1&pageSize=-1",
-        "http://www.sephora.com/luminizer-luminous-makeup?defaultSortBy=-1&pageSize=-1", "http://www.sephora.com/face-brushes-makeup-brushes-applicators-makeup?defaultSortBy=-1&pageSize=-1"
-    ]
+    base_url = "http://www.sephora.com/"
+    sort_query = "?defaultSortBy=-1&pageSize=-1"
+    components = ["foundation-sets","foundation-makeup","tinted-moisturizer","bb-cc-cream-face-makeup","makeup-primer-face-primer","setting-powder-face-powder","concealer","blush-face-makeup","bronzer-makeup","luminizer-luminous-makeup","blotting-paper-oil-control","makeup-remover","sponges-applicators-makeup-brushes-applicators-makeup","face-brushes-makeup-brushes-applicators-makeup","eyeshadow-palettes","mascara-eyes-makeup","eyeliner","eyeshadow","eyeshadow-primer-eye-primer","under-eye-concealer","eyebrow-makeup-pencils","eyelash-enhancers-eyelash-primer","eyelash-curlers-eyes-makeup","fake-eyelashes-false-eyelashes","eye-makeup-remover","eye-brushes-makeup-brushes-applicators-makeup","lip-palettes-lipstick-palettes","lipstick","lip-plumper","lip-stain","lip-liner-lip-pencils","lip-balm-treatments-lips-makeup","lip-brushes-makeup-brushes-applicators-makeup","makeup-palettes","makeup-value-sets","brush-sets-makeup-brushes-applicators-makeup"]
+    #components = ["foundation-sets"]
+
+    start_urls = [base_url + component + sort_query for component in components];
+
+
 
     def parse(self, response):
         sel = Selector(response)
